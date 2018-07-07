9f67d9fd0e6deee609a2eaf7482358057b75df11
Tony <tony@gmail.com>
2018-07-07 17:49:43 +0800

    AsyncTask
    服务demo更新了一部分

--- ServiceBestPractice/app/src/main/java/com/example/ericzhang/servicebestpractice/DownloadTask.java
+++ ServiceBestPractice/app/src/main/java/com/example/ericzhang/servicebestpractice/DownloadTask.java
@@ -13,6 +13,18 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+/*
+
+   为了保证DownloadTask可以一直在后台运行，我们需要创建下载服务
+
+
+ */
+
+
+
+//三个参数，1、执行AsyncTask，需要传入一个字符串参数给后台任务
+//2.作为进度显示单位
+//3.使用整型数据作为反馈结果
 public class DownloadTask extends AsyncTask<String,Integer,Integer> {
 
     public static final int TYPE_SUCCESS = 0;
@@ -44,10 +56,14 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
 
         try{
             long downloadedLength = 0;//记录已下载的文件长度
+
+            //我们从参数中获取到下载的URL地址，并根据URL地址解析出文件名
             String downloadUrl = params[0];
 
             String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
 
+
+            //将文件下载到sd卡中的download目录下
             String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
             //拼了一个新的地址
             file = new File(directory + fileName);
@@ -67,12 +83,15 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
 
             }
             OkHttpClient client = new OkHttpClient();
+            //加了个请求头，用于高速服务器想从哪个字节开始下载
             Request request = new Request.Builder()
                     .addHeader("RANGE","bytes=" + downloadedLength + "-")
                     .url(downloadUrl)
                     .build();
             Response response = client.newCall(request).execute();
             if (response != null){
+
+
                 is = response.body().byteStream();
                 savedFile = new RandomAccessFile(file,"rw");
                 //跳过已下载的字节
@@ -81,9 +100,11 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
                 byte[] b = new byte[1024];
                 int total = 0;
                 int len;
-
+                //如果还能读出来1k字节，就一直往里边写
                 while((len = is.read(b)) != -1){
 
+
+                    //取消和暂停方法被调用了
                     if (isCanceled){
 
                         return  TYPE_CANCELED;
@@ -94,7 +115,7 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
                     }else{
 
                         total += len;
-
+                        //off是字节数组的开始index
                         savedFile.write(b,0,len);
                         //计算已下载的百分比
                         int progress = (int) ((total + downloadedLength) * 100 /connectLength);
@@ -102,6 +123,7 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
 
                     }
                 }
+                //
                 response.body().close();
                 return  TYPE_SUCCESS;
 
@@ -138,6 +160,64 @@ public class DownloadTask extends AsyncTask<String,Integer,Integer> {
 
         return TYPE_FAILED;
     }
+
+
+    //publishProgress(progress)调用的时候，这个方法会被连带调用
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+
+        int progress = values[0];
+        if (progress > lastProgress){
+
+            //给监听传值，方便更新
+            listener.onProgress(progress);
+            lastProgress = progress;
+
+        }
+
+    }
+
+    //根据asyncTask中传入的下载状态来进行回调
+    @Override
+    protected void onPostExecute(Integer integer) {
+
+        switch (integer){
+
+            case TYPE_SUCCESS:
+                listener.onSuccess();
+                break;
+            case TYPE_FAILED:
+                listener.onFailed();
+                break;
+            case TYPE_PAUSED:
+                listener.onPaused();
+                break;
+            case TYPE_CANCELED:
+                listener.onCanceled();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+
+    //暂停和取消
+    //暂停
+    public void pauseDownload(){
+
+        isPaused = true;
+
+    }
+
+    //取消
+    public void cancelDownload(){
+
+        isCanceled = true;
+
+    }
+
+
 
     private long getContentLength(String downloadUrl) throws IOException {
 
