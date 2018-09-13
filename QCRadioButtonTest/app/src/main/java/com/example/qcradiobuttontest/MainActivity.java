c9e0d24ce6e2f4e0aebe91bfe68da01a3ee3a72e
Tony <tony@gmail.com>
2018-09-13 11:24:45 +0800

    RadioGroup
    复选框

---
+++ QCRadioButtonTest/app/src/main/java/com/example/qcradiobuttontest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.qcradiobuttontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RadioGroup radgroup = (RadioGroup) findViewById(R.id.radioGroup);
+        //第一种获得单选按钮值的方法
+        //为radioGroup设置一个监听器:setOnCheckedChanged()
+        radgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radbtn = (RadioButton) findViewById(checkedId);
+                Toast.makeText(getApplicationContext(), "按钮组值发生改变,你选了" + radbtn.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
